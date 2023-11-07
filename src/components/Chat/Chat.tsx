f7d6b2175e7f642bfe0ea355c0f7e186cba7a5da
Juan Jose Gomez Medina <juanjo.gomez2000@hotmail.com>
2023-11-07 13:49:54 -0500

    structure started

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,10 @@
+import './Chat.css'
+
+export default function Chat() {
+  return (
+    <main id='chat-container'>
+        <p>this is where the chat will be</p>
+    </main>
+  )
+}
+
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,17 @@
+import "./Sidebar.css"
+
+const Sidebar = () => {
+    return(
+        <aside>
+            <ul>
+                <li>home</li>
+                <li>about</li>
+                <li>contact</li>
+                <li>blog</li>
+                <li>portfolio</li>
+            </ul>
+        </aside>
+    )
+}
+
+export default Sidebar;
